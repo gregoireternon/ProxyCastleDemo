9b478309c8df1c82b6293cfb3eeafe628bc74442
Maxime Lentz <maxime.lentz@viacesi.fr>
2019-09-16 17:08:33 +0200

    Ajout programme pour évaluer les performances du proxy

---
+++ VegetablesEnumerator/ProxyPerformance/ProxyTools/BasicInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using Castle.DynamicProxy;
+
+namespace ProxyPerformance.ProxyTools
+{
+    public class BasicInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            invocation.Proceed();
+        }
+    }
+}
